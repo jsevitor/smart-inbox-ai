327d14dcb7c088d4b8bb92f72db11f1dc5e0a447
jsevitor <vitorjseo@gmail.com>
2025-08-31 20:37:51 -0300

    feat(classifier): Adiciona logica de classificacao com modelo Google Gemini

--- app/backend/modules/classifier.py
+++ app/backend/modules/classifier.py
@@ -1,24 +1,49 @@
-from transformers import pipeline
-from modules.preprocess import SpacyPreprocessor  # supondo que salve em preprocess_spacy.py
+import json
+import re
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+from modules.preprocess import SpacyPreprocessor
+
+load_dotenv()
+API_KEY = os.getenv("GOOGLE_GENAI_API_KEY")
 
 class EmailClassifier:
-    def __init__(self, model_name="facebook/bart-large-mnli", language="pt"):
-        self.labels = ["Produtivo", "Improdutivo"]
-        self.classifier = pipeline(
-            "zero-shot-classification",
-            model=model_name,
-            tokenizer=model_name
-        )
+    def __init__(self, language="pt"):
+        if not API_KEY:
+            raise ValueError("Chave da API não encontrada no .env")
+        self.client = genai.Client(api_key=API_KEY)
         self.preprocessor = SpacyPreprocessor(language=language)
 
     def classify(self, text: str):
-        # pré-processa o texto antes de classificar
         clean_text = self.preprocessor.preprocess(text)
 
-        result = self.classifier(clean_text, candidate_labels=self.labels)
-        return {
-            "texto_original": text,
-            "texto_preprocessado": clean_text,
-            "categoria": result["labels"][0],
-            "confianca": result["scores"][0]
-        }
+        prompt = f"""
+        Analise o texto a seguir retirado de um email. 
+        Classifique-o em categorias: 'Produtivo' ou 'Improdutivo'. 
+        Retorne SOMENTE um JSON com a chave "categoria" e "confianca" (valor entre 0 e 1).
+        Não adicione texto extra.
+        Texto: "{clean_text}"
+        """
+
+        response = self.client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt,
+            config=types.GenerateContentConfig(
+                thinking_config=types.ThinkingConfig(thinking_budget=0)
+            ),
+        )
+
+        # remove ```json ... ``` caso exista
+        cleaned_text = re.sub(r"^```json\s*|\s*```$", "", response.text.strip(), flags=re.IGNORECASE)
+
+        try:
+            resultado = json.loads(cleaned_text)
+            # garante que confianca seja float entre 0 e 1
+            if "confianca" in resultado:
+                resultado["confianca"] = float(resultado["confianca"])
+        except (json.JSONDecodeError, ValueError, TypeError):
+            resultado = {"categoria": None, "confianca": None}
+
+        return resultado
